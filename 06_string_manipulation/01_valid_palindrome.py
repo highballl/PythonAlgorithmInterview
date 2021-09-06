1f69b5ae2834f5a1a27c40ca22c19d90c2304af6
parkjiyoon <jyp0293@gmail.com>
2021-09-06 20:26:32 +0900

    add: 06_string_manipulation folder and add: 01_valid_palindrome

---
+++ 06_string_manipulation/01_valid_palindrome.py
@@ -0,0 +1,30 @@
+from collections import deque
+import sys
+sentence = sys.stdin.readline().strip().lower()
+
+special_char = "\"\'[!@#$%^&*:()\,]"
+new_sentence = deque([])
+for word in sentence:
+    new_word = word.lower()
+    for char in new_word:
+        if char in special_char or char == ' ':
+            pass
+        else:
+            new_sentence.append(char)
+#print(new_sentence)
+
+def is_valid(sentence):
+    while new_sentence:
+        left = new_sentence.popleft()
+        if new_sentence:
+            right = new_sentence.pop()
+            if left == right:
+                pass
+            else:
+                return "false"
+                
+    return "true"
+
+
+    
+print(is_valid(new_sentence))
