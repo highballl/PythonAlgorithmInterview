c005c73b1f034afbb02d23ce567c2e2236a6db80
parkjiyoon <jyp0293@gmail.com>
2021-09-07 15:33:06 +0900

    fix: 03_reorder_data_in_log_files.py

--- 06_string_manipulation/03_reorder_data_in_log_files.py
+++ 06_string_manipulation/03_reorder_data_in_log_files.py
@@ -1,15 +1,5 @@
-# import sys
-
-# logs = sys.stdin.readline().strip().split(',')
-# #print(logs)
-
-# class Solution:
-#     def reorderLogFiles(self, logs: List[str]) -> List[str]:
-#         pass
-
-
 logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
-#logs = ["dig2 3 6", "dig1 8 1 5 1","let1 art can","let2 own kit dig","let3 art zero"]
+
 digit = []
 letter = []
 count = 0
@@ -33,16 +23,3 @@ sorted_letter = sorted(letter, key=lambda x: (x[1], x[0]))
 letter = [sorted_letter[i][0]+sorted_letter[i][1] for i in range(count)]
 print([*letter, *digit])
 
-
-# temp = []
-# for log in logs:
-#     seperated = log.split()
-#     identifier = seperated[0]
-#     rest = ' '.join(seperated[1:])
-#     temp.append([identifier, rest])
-
-# print(temp[0][1])
-
-# temp = sorted(temp, key = lambda x: x[1])
-
-# print(temp)
